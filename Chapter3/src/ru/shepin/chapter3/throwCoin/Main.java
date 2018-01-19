613186fc104b17a0a2f25edca3367c7cb4bdb082
Dmitry <bright1308@gmail.com>
2018-01-19 19:01:19 +0700

    second

--- Chapter3/src/ru/shepin/chapter3/throwCoin/Main.java
+++ Chapter3/src/ru/shepin/chapter3/throwCoin/Main.java
@@ -3,13 +3,7 @@ package ru.shepin.chapter3.throwCoin;
 public class Main {
     public static void main(String[] args) {
         int eagle = 1;
-
         int randomNumber = (int) (Math.random() * 2);
-
-        if (randomNumber == eagle) {
-            System.out.println("eagle");
-        } else {
-            System.out.println("tails");
-        }
+        System.out.println(randomNumber == eagle ? "eagle" : "tails");
     }
 }
